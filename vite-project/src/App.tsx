a7b5d8f66a9bd3089d8c0696d7dafcb5cca1866e
Joseph Baruch <josephbaruch48@gmail.com>
2023-10-07 23:47:58 -0700

    Trying to figure things out

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,18 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
+    return item;
   };
 
   return (
     <div>
       <ListGroup
         items={items}
-        heading="Cities"
+        heading={"cities"}
         onSelectItem={handleSelectItem}
       />
     </div>
--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -13,7 +13,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
 
   return (
     <>
-      <h1>{heading}</h1>
+      <h1>{items && heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
