2004b6195c94c8b9d9be0d0944a717d6000376b5
Joseph Baruch <josephbaruch48@gmail.com>
2023-10-18 11:18:47 -0700

    Working on setting up pre-fetch call

--- next.js/learn/src/pages/index.tsx
+++ next.js/learn/src/pages/index.tsx
@@ -1,29 +1,61 @@
 import Head from 'next/head';
 import React from 'react';
 
+    const side = {
+        display: 'flex',
+    };
+
+    const labelStyles = {
+      marginBottom: '10px', // Add spacing between labels
+    };
+
+    const ipInput = {
+        width: '100px',
+    };
+
+    const inp = {
+        width: '50px',
+    };
+
 class Home extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             hit: false,
-            ip: "IP Address", 
+            ip: "IP Address",
+            count: 'Count',
+            timeout: "Timeout", 
         };
+    }
+
+    fetchIPPing = () => {
+        console.log("hello");
     }
 
     handleClick = () => {
         this.setState({
             hit: true, 
         });
+        this.fetchIPPing();
     }
 
-    handleChange = (event) => {
+    handleIPChange = (event) => {
         this.setState({
             hit: false,
             ip: event.target.value,
         });
     }
-    doit(){
-        console.log("hello");
+    handleCountChange = (event) => {
+        this.setState({
+            hit: false,
+            count: event.target.value,
+        });
+    }
+    handleTimeChange = (event) => {
+        this.setState({
+            hit: false,
+            timeout: event.target.value,
+        });
     }
 
     render(){
@@ -33,15 +65,35 @@ class Home extends React.Component {
                     <title>Learning React</title>
                 </Head>
                 <h1>Diagnostics</h1>
-                <label>
-                    <input
-                        type="text"
-                        value={this.state.ip} 
-                        onChange={this.handleChange}
-                    />
+                <div id="pingIP" styles={side}>
+                    <h2>Ping IP</h2>
+                    <label style={labelStyles}>
+                        <input
+                            style={ipInput}
+                            type="text"
+                            value={this.state.ip} 
+                            onChange={this.handleIPChange}
+                        />
+                    </label>
+                    <label  style={labelStyles}>
+                        <input
+                            style={inp}
+                            type="text"
+                            value={this.state.count} 
+                            onChange={this.handleCountChange}
+                        />
+                    </label>
+                    <label  style={labelStyles}>
+                        <input
+                            style={inp}
+                            type="text"
+                            value={this.state.timeout} 
+                            onChange={this.handleTimeChange}
+                        />
+                    </label>
                     <button onClick={this.handleClick}>Ping</button>
-                </label>
-                {this.state.hit && this.state.ip} 
+                    {this.state.hit && (<h1>{this.state.ip} {this.state.count} {this.state.timeout}</h1>)}
+                </div>
             </>
         );
     }
